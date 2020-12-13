7a294ed0d75685718ae844f49071d355bdf428be
Joey-Coder <joey950326@gmail.com>
2020-12-13 16:35:53 +0800

    init demo-ui

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/essential', '@vue/standard'],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 取消函数名和括号之间需要加空格的要求
+    'space-before-function-paren': 0
+  }
+}
